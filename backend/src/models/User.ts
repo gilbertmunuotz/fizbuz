05feb0768f391a3f0e83abe3392aad2f62cfcb4c
gilbertmunuotz <officialgilbert24@gmail.com>
2024-10-12 22:24:37 +0300

    minor updates v 1.4.0

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,22 +1,3 @@
-import moment from 'moment';
-
-
-// **** Variables **** //
-
-const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an object ' + 
-  'with the appropriate user keys.';
-
-
-// **** Types **** //
-
-export interface IUser {
-  id: number;
-  name: string;
-  email: string;
-  created: Date;
-}
-
-
 // **** Functions **** //
 
 /**
