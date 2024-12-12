910bf5abca1a17e37d8711489fb43d84603a87a5
gilbertmunuotz <officialgilbert24@gmail.com>
2024-12-12 18:13:51 +0300

    added logic where no transaction is found🙃🙃

--- frontend/src/pages/Transaction.tsx
+++ frontend/src/pages/Transaction.tsx
@@ -11,7 +11,7 @@ export default function Transaction() {
 
     // Extract user information & Annotate it 
     const userInfo = useSelector(user) as AuthResponse;
-    
+
 
     // Grab the Id from userInfo Object
     const userId = userInfo.id;
@@ -41,22 +41,26 @@ export default function Transaction() {
             <div className="lg:ml-64 mx-6 my-4">
                 <h1 className="ml-8 font-serif text-2xl mb-3">My Transactions</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ml-4">
-                    {transactions?.transactions.map((transaction) => (
-                        <div key={transaction.id} className={` border-l-2 p-3 rounded shadow-sm flex items-center justify-between ${transaction.type === 'income' ? 'border-blue-500' : 'border-yellow-500'}`}>
-                            <div>
-                                <h2 className="text-lg font-semibold">{transaction.name}</h2>
-                                <p className="text-gray-700">
-                                    Amount: <span className="font-bold">${transaction.amount}</span>
-                                </p>
+                    {transactions ? (
+                        transactions?.transactions.map((transaction) => (
+                            <div key={transaction.id} className={` border-l-2 p-3 rounded shadow-sm flex items-center justify-between ${transaction.type === 'income' ? 'border-blue-500' : 'border-yellow-500'}`}>
+                                <div>
+                                    <h2 className="text-lg font-semibold">{transaction.name}</h2>
+                                    <p className="text-gray-700">
+                                        Amount: <span className="font-bold">${transaction.amount}</span>
+                                    </p>
 
-                                <p className={`text-sm font-medium mt-2 ${transaction.type === 'income' ? 'text-blue-500' : 'text-yellow-500'}`}>
-                                    {transaction.type.charAt(0).toUpperCase() + transaction.type.slice(1)}
-                                </p>
+                                    <p className={`text-sm font-medium mt-2 ${transaction.type === 'income' ? 'text-blue-500' : 'text-yellow-500'}`}>
+                                        {transaction.type.charAt(0).toUpperCase() + transaction.type.slice(1)}
+                                    </p>
+                                </div>
+
+                                <button onClick={() => handleDelete(transaction.id)}> <DeleteIcon sx={{ cursor: 'pointer', color: 'red' }} /></button>
                             </div>
-
-                            <button onClick={() => handleDelete(transaction.id)}> <DeleteIcon sx={{ cursor: 'pointer', color: 'red' }} /></button>
-                        </div>
-                    ))}
+                        ))
+                    ) : (
+                        <h5 className='text-lg sm:ml-5'>No Transaction Found! 🙃</h5>
+                    )}
                 </div>
             </div>
         </>
