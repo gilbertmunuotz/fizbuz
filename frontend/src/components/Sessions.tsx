423775f4c7cac30d21d6bb97a833f08d39f0686d
gilbertmunuotz <officialgilbert24@gmail.com>
2025-01-01 22:40:44 +0300

    installed ua-parser.js library

--- frontend/src/components/Sessions.tsx
+++ frontend/src/components/Sessions.tsx
@@ -1,9 +1,8 @@
+import { UAParser } from "ua-parser-js";
 import { useSelector } from "react-redux";
 import { user } from "../assets/authSlice";
-import { formatDate } from "../utilities/DateFunc";
 import { AuthResponse } from "../Interfaces/interface";
 import { useUseSessionsQuery } from "../api/AuthSlice";
-
 
 export default function Sessions() {
 
@@ -18,6 +17,17 @@ export default function Sessions() {
     // Extract the first session object safely
     const session = data?.user?.[0];
 
+    // Grab the user agent string
+    const userAgent = data?.user?.[0].userAgent;
+
+    // Pass the user agent string to the parser
+    const parser = new UAParser(userAgent);
+
+    // Extract the browser, device and OS information
+    const browser = parser.getBrowser();
+    const os = parser.getOS();
+    const device = parser.getDevice();
+
     return (
         <div>
             <h1 className="font-semibold text-lg">Web Sessions</h1>
@@ -31,12 +41,16 @@ export default function Sessions() {
                     <div className="bg-gray-100 p-4 rounded-lg border">
                         <div className="flex items-center justify-between">
                             <div>
+
                                 <p className="text-sm text-gray-500">Your current session</p>
                                 <div className="flex justify-between">
                                     <span className="text-green-500">● Active</span>
                                     <h2 className="font-semibold text-gray-800">{session.ipAddress}</h2>
                                 </div>
-                                <p className="text-sm text-gray-500">Last Seen: {formatDate(session.updatedAt!)}</p>
+                                {/* Display parsed details */}
+                                <p className="text-sm text-gray-500">
+                                    {browser.name} on {os.name} {os.version} {device.type && `on ${device.type}`}
+                                </p>
                             </div>
                         </div>
                     </div>
--- frontend/src/utilities/DateFunc.ts
+++ frontend/src/utilities/DateFunc.ts
@@ -1,11 +1,11 @@
-// Utility function for date formatting
-export const formatDate = (date: string) => {
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-    }
-    return new Date(date).toLocaleString(undefined, options);
-};
+// // Utility function for date formatting
+// export const formatDate = (date: string) => {
+//     const options: Intl.DateTimeFormatOptions = {
+//         year: 'numeric',
+//         month: 'short',
+//         day: 'numeric',
+//         hour: '2-digit',
+//         minute: '2-digit',
+//     }
+//     return new Date(date).toLocaleString(undefined, options);
+// };
