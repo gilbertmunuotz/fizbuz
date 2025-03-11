292bb89b0a9d36918d637a702160b971225fde00
gilbertmunuotz <officialgilbert24@gmail.com>
2025-03-12 02:51:16 +0300

    configured app for production

--- frontend/src/config/constant.ts
+++ frontend/src/config/constant.ts
@@ -1,1 +1,2 @@
-export const SERVER_API = "http://localhost:8000/api";
+// export const SERVER_API = "http://localhost:8000/api";
+export const SERVER_API = "https://fizbuz-api-production.up.railway.app/api";
